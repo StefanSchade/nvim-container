7cc04bfd8f36681fe806be37d12aec54c936aa08
Dr. Stefan Schade <dr_stefan_schade@yahoo.com>
2024-07-16 18:23:18 +0200

    Working on the tool configuration
    Refs #3

--- scripts/postCreateScript.sh
+++ scripts/postCreateScript.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-# Source and call helper script
+# Log the script name
 source /mnt/dev-env-setup/scripts/helper/log_helper.sh && log_script_name
 
-# Check for and set up Zscaler certificate
+# Setup ZScaler certificate if available
 source /mnt/dev-env-setup/scripts/setup_zscaler.sh
 
 # Execute additional setup scripts
 echo "Executing setup_ssh.sh"
 source /mnt/dev-env-setup/scripts/setup_ssh.sh || { echo "Failed to execute setup_ssh.sh"; exit 1; }
 
-# Any other setup tasks can be added here
+# Perform tool setups
+echo "Executing configure_tools.sh with -s default"
+/mnt/dev-env-setup/scripts/configure_tools.sh -s default || { echo "Failed to execute configure_tools.sh"; exit 1; }
+
+# Create a symbolic link for the scripts directory
+ln -s /mnt/dev-env-setup/scripts ~/scripts || { echo "Failed to create symbolic link"; exit 1; }
--- scripts/setup_ssh.sh
+++ scripts/setup_ssh.sh
@@ -1,26 +1,43 @@
 #!/bin/bash
 
+# Create .ssh directory if it doesn't exist
+mkdir -p /home/devuser/.ssh
+chmod 700 /home/devuser/.ssh
+
 # Start the SSH agent
 eval "$(ssh-agent -s)"
 
-# Fix permissions of the private key files
-chmod 600 /home/devuser/.ssh/*_rsa
+# Fix permissions of the private key files if they exist
+if ls /home/devuser/.ssh/*_rsa 1> /dev/null 2>&1; then
+    chmod 600 /home/devuser/.ssh/*_rsa
+else
+    echo "No SSH private keys found in /home/devuser/.ssh"
+fi
 
 # Fix permissions of the .ssh/config file if it exists
 if [ -f /home/devuser/.ssh/config ]; then
     chmod 600 /home/devuser/.ssh/config
 fi
 
-# Add all SSH keys to the agent
-for key in /home/devuser/.ssh/*_rsa; do
-    ssh-add $key
-done
+# Add all SSH keys to the agent if they exist
+if ls /home/devuser/.ssh/*_rsa 1> /dev/null 2>&1; then
+    for key in /home/devuser/.ssh/*_rsa; do
+        ssh-add $key
+    done
+else
+    echo "No SSH private keys found to add to the agent."
+fi
 
-# Remove the offending key for GitHub from known_hosts
-ssh-keygen -f "/home/devuser/.ssh/known_hosts" -R "github.com"
+# Ensure known_hosts file exists
+touch /home/devuser/.ssh/known_hosts
+chown devuser:devuser /home/devuser/.ssh/known_hosts
+chmod 644 /home/devuser/.ssh/known_hosts
+
+# Remove the offending key for GitHub from known_hosts if it exists
+ssh-keygen -f "/home/devuser/.ssh/known_hosts" -R "github.com" || true
 
 # Add GitHub's new RSA key to known_hosts
-ssh-keyscan -t rsa github.com >> /home/devuser/.ssh/known_hosts
+ssh-keyscan -H github.com >> /home/devuser/.ssh/known_hosts
 
 # List all identities added to the SSH agent
 echo "SSH Identities:"
